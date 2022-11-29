76bd167e5b85d058ed693cbfc294c12d90cbb807
Cosmin Miroiu <miroiucosmin93@gmail.com>
2022-11-29 20:16:03 +0200

    Added main packages

---
+++ src/test/java/com/cosminmiroiu/myprofile/MyprofileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosminmiroiu.myprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprofileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
